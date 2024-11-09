1c9a831b8278fb016aded5e96a137dd7371a5f5a
AmirHosein Hoseini <amirhossein.hossseiini1372@gmail.com>
2024-11-08 22:02:41 -0800

    fixed main

--- server/main.js
+++ server/main.js
@@ -10,8 +10,8 @@ async function main() {
   const app = express();
   const port = process.env.PORT;
   require("./src/config/mongoose.config");
-  app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
   SwaggerConfig(app);
   app.use(mainRouter);
   notFound(app);
